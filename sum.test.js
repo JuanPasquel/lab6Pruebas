ed611433d073bd36bca002d3c386b53fbaaf53bf
JuanDavidPasquelRuiz <132899302+JuanPasquel@users.noreply.github.com>
2025-08-07 11:51:18 -0500

    Proyecto base con integracion con lint

---
+++ sum.test.js
@@ -0,0 +1,9 @@
+const suma = require('../src/sum')
+
+
+test ('Suma de 2 + 3 debe ser 5', () => {
+    expect(suma(2, 3)).toBe(5)
+})
+test ('Suma de 10 + 5 debe ser 15', () => {
+    expect(suma(10, 5)).toBe(15)
+})
