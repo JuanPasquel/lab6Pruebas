9a1b90e1eb1b47028ed6a2b20029f2173c6097f6
JuanDavidPasquelRuiz <132899302+JuanPasquel@users.noreply.github.com>
2025-08-09 23:07:49 -0500

    Add Hola Mundo GitHub Actions workflow

---
+++ cam.py
@@ -0,0 +1,41 @@
+#TALLER DETECCIÓN DE ROSTROS EN VIVO
+
+import cv2
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Captura video desde la cámara principal (1)
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: No se pudo acceder a la cámara.")
+    exit()
+
+
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Error: No se pudo capturar el frame.")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow('Detección de Rostros - Haar Cascade', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
